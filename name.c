a2193f515be689c310db90586c7c828ca2b9ec7a
manishsilpakar <manish.silpakar2010@gmail.com>
2016-11-23 10:39:29 +0545

    Add files via upload

---
+++ name.c
@@ -0,0 +1,25 @@
+/*
+name:manish silpakar
+subject:programming fundamental
+roll no:
+program:write a program to find sum of digit of three digits number
+lab sheet no:5
+date:/23/11/2016
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float a,b,c,sum;
+printf("enter the first digit number\n");
+scanf("%f",&a);
+printf("enter the second digit number\n");
+scanf("%f",&b);
+printf("enter the third digit number\n");
+scanf("%f",&c);
+sum=a+b+c;
+printf("the sum of three digit number is %.2f",sum);
+return 0;
+}
+
