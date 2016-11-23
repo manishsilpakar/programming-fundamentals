b01badcbe8ebcd334261742b28c246d67eb7eec1
manishsilpakar <manish.silpakar2010@gmail.com>
2016-11-23 12:33:05 +0545

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,21 @@
+/*
+name:manish silpakar
+subject:programming fundamental
+roll no:
+program:to find ASCII value of any character assigned from user
+lab sheet no:5
+date:23/11/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char c;
+    int asciivalue;
+    printf("enter the character value:");
+    scanf("%c",&c);
+    printf("ASCII value of %c=%d",c,c);
+    return 0;
+
+}
