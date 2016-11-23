3ce9eb38a118840cf89c81b53105521682b19bb1
manishsilpakar <manish.silpakar2010@gmail.com>
2016-11-23 10:39:52 +0545

    Add files via upload

---
+++ read temperature.c
@@ -0,0 +1,21 @@
+/*
+name:manish silpakar
+subject:programming fundamental
+roll no:
+program:write a program to find sum of digit of three digits number
+lab sheet no:5
+date:/23/11/2016
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float c,f;
+printf("enter the temperature in fahrenhit\n");
+scanf("%f",&f);
+c=(0.56*(f-32));
+printf("the temperature in centigrade is %.2f",c);
+return 0;
+}
+
