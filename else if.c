e1acba0f5ea8486f8382dff73580e2c8623597aa
manishsilpakar <manish.silpakar2010@gmail.com>
2016-11-24 11:24:13 +0545

    Add files via upload

---
+++ else if.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter a number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("number is positive");
+}
+else
+{
+  printf("number is negative");
+}
+
+getch();
+return 0;
+}
