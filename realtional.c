1bfba4ef84d06d10c35f8c42c6ad9e3a76d9627f
manishsilpakar <manish.silpakar2010@gmail.com>
2016-11-28 11:09:54 +0545

    Add files via upload

---
+++ realtional.c
@@ -0,0 +1,40 @@
+/*
+name:manish silpakar
+subject:programming fundamental
+roll no:
+program: to enter marks of 7 subjects
+date:28/11/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    printf("enter the first number:\n");
+    scanf("%d",&a);
+    printf("enter the second number:\n");
+    scanf("%d",&b);
+   a==b;
+   printf("\nin use of == %d\n",a==b);
+
+    a>b;
+    printf("\nin use of > %d\n",a>b);
+
+   a<b;
+   printf("\nin use of < %d\n",a<b);
+
+    a!=b;
+    printf("\nin use of ! %d\n",a!=b);
+
+   a>=b;
+   printf("\nin use of >= %d",a>=b);
+
+    a<=b;
+    printf("\nin use of <= %d\n",a<=b);
+
+    getch();
+    return 0;
+
+
+}
