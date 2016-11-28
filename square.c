cb31aa455c02769af4f4320352b7b71b11f482b5
manishsilpakar <manish.silpakar2010@gmail.com>
2016-11-28 12:39:44 +0545

    Add files via upload

---
+++ square.c
@@ -0,0 +1,24 @@
+/*
+name:manish silpakar
+subject:programming fundamental
+roll no:
+program:write a program to find a square root of number
+lab sheet no:5
+date:/28/11/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+float n, sqr=0,sqrt=0;
+printf("enter a number:\n");
+scanf("%f",&n);
+sqr=n*n;
+sqrt=pow(n,0.5);
+printf("square of number is:%f\n",sqr);
+printf("square root of number is:%f",sqrt);
+getch();
+return 0;
+}
