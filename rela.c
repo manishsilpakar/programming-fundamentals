ee6e2d066582582593cca88fd5089358dacd586e
manishsilpakar <manish.silpakar2010@gmail.com>
2016-12-05 11:31:25 +0545

    Add files via upload

---
+++ rela.c
@@ -0,0 +1,41 @@
+/*
+name:manish silpakar
+subject:programming fundmental
+program:
+roll no:18
+lab sheet no:10
+date:5/12/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float a;
+    printf("enter the first number:\n");
+    scanf("%f",&a);
+    if(a>=40)
+    {
+        printf("\ncongratulation you have got 3rd division");
+
+    if (a>=50)
+    {
+        printf("\ncongratulation you have got 2nd division");
+
+    if (a>=60)
+    {
+        printf("\ncongratulation you have got 1st division");
+
+    if (a>=80)
+    {
+        printf("\ncongratulation you have got distinction");
+
+    }
+    }
+    }
+    }
+
+    getch();
+    return 0;
+
+}
