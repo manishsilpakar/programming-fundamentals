9330692c5c4546bd8d9b779929e67f7e592a86a2
manishsilpakar <manish.silpakar2010@gmail.com>
2016-12-07 11:43:44 +0545

    Add files via upload

---
+++ ternary odd or even.c
@@ -0,0 +1,21 @@
+/*
+name:manish silpakar
+roll no:18
+subject:programming fundamental
+program: to find odd or even number using ternary operator
+lab sheet no:11
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a;
+printf("enter the first number:");
+scanf("%d",&a);
+(a%2==0)?printf("\nthe number of a is even",a):printf("\nthe number of a is odd",a);
+getch();
+return 0;
+}
+
+
