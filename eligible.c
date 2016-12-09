58e8cd074239548447e5226734b1b325dc2f7f8d
manishsilpakar <manish.silpakar2010@gmail.com>
2016-12-09 11:07:44 +0545

    Add files via upload

---
+++ eligible.c
@@ -0,0 +1,18 @@
+/*
+name:manish silpakar
+roll no:18
+program: to print eligible to vote
+subject:proramming fundamental
+lab sheet no:12
+date:09\12\2016
+*/
+
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the age of person:");
+scanf("%d",&a);
+(a>=18)?printf("it is eligble to vote"):printf("it is not eligible to vote");
+return 0;
+}
